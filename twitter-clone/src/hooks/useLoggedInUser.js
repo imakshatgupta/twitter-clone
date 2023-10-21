03fd85b2ef5e4e955c0491296555404c85105d6c
Akshat Gupta <akshatgtc@gmail.com>
2023-10-22 00:56:49 +0530

    change

--- twitter-clone/src/hooks/useLoggedInUser.js
+++ twitter-clone/src/hooks/useLoggedInUser.js
@@ -7,7 +7,7 @@ const useLoggedInUser = () => {
     const [loggedInUser, setLoggedInUser] = useState({});
 
     useEffect(() => {
-        fetch(`https://pacific-peak-30751.herokuapp.com/loggedInUser?email=${email}`)
+        fetch(`http://localhost:5000/loggedInUser?email=${email}`)
             .then(res => res.json())
             .then(data => {
                 // console.log('from useLoggedinuser', data)
