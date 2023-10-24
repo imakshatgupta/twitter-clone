90f283df9aef4cbd1f25f31ff698dc8390da96af
Akshat Gupta <akshatgtc@gmail.com>
2023-10-24 16:49:34 +0530

    added premium cards

--- twitter-clone/src/pages/Premium/Premium.js
+++ twitter-clone/src/pages/Premium/Premium.js
@@ -1,28 +1,27 @@
-import React , {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useUserAuth } from "../../context/UserAuthContext";
 
 import "./Premium.css";
 
 const Premium = () => {
   const { user } = useUserAuth();
+  const email = user?.email;
 
   const [aplan, setAplan] = React.useState("");
   const [plan, setPlan] = React.useState("");
 
-
   useEffect(() => {
-      fetch('http://localhost:5000/loggedInUser')
-          .then(res => res.json())
-          .then(data => {
-              setAplan(data[0]);
-          })
-  }, [aplan])
+    fetch(`http://localhost:5000/loggedInUser?email=${email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setAplan(data[0]?.plan);
+      });
+  }, [aplan]);
 
   console.log(aplan);
 
-
   const makePaymentSilver = () => {
-    setPlan("2");
+    setPlan("3");
     const editedInfo = {
       plan,
     };
@@ -36,37 +35,44 @@ const Premium = () => {
     })
       .then((res) => res.json())
       .then((data) => {
-        // console.log(data);
-
+        console.log(data);
       });
   };
 
   return (
     <div className="subscriptions-container">
-      <h2>Choose Your Plan</h2>
-      <div className="plans">
+      {aplan === "1" ? (
+        <>
+          <h2>You are on Free Plan. Upgrade now.</h2>
+          <div className="plan-card" onClick={makePaymentSilver}>
+            <h3>Silver Plan</h3>
+            <p>₹100/month</p>
+            <p>Tweets Per Day: 5</p>
+          </div>
+          <div className="plan-card">
+            <h3>Gold Plan</h3>
+            <p>₹1000/month</p>
+            <p>Tweets Per Day: Unlimited</p>
+          </div>
+        </>
+      ) : aplan === "2" ? (
+        <>
+        <h1>You are on Silver Plan. Upgrade now.</h1>
         <div className="plan-card">
-          <h3>Free Plan</h3>
-          <p>Free</p>
-          <p>Tweets Per Day: 1 </p>
-        </div>
-
-        <div className="plan-card" onClick={ makePaymentSilver}>
-          <h3>Silver Plan</h3>
-          <p>₹100/month</p>
-          <p>Tweets Per Day: 5 </p>
-        </div>
-        <div
-          className="plan-card"
-          // onClick={() => makePaymentGold }
-        >
-          <h3>Gold Plan</h3>
-          <p>₹1000/month</p>
-          <p>Tweets Per Day: Unlimited </p>
-        </div>
-      </div>
+            <h3>Gold Plan</h3>
+            <p>₹1000/month</p>
+            <p>Tweets Per Day: Unlimited</p>
+          </div>
+</>
+      ) : aplan === "3" ? (
+        <h1>You are on Gold Plan. Enjoy!!</h1>
+      ) 
+      : (
+        <h1>Loading...</h1>
+      )}
     </div>
   );
-};
+}
+
 
 export default Premium;
