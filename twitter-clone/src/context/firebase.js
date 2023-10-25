e8b3dbf612ad1c065ec7e4c3252aef570e5dee7f
Akshat Gupta <akshatgtc@gmail.com>
2023-10-25 23:13:52 +0530

    login validation

--- twitter-clone/src/context/firebase.js
+++ twitter-clone/src/context/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyC1UsxjORHhL9LfqNkl_wsTJ2rV3eDSvy0",
-    authDomain: "twitter-clone-eb901.firebaseapp.com",
-    projectId: "twitter-clone-eb901",
-    storageBucket: "twitter-clone-eb901.appspot.com",
-    messagingSenderId: "434624553161",
-    appId: "1:434624553161:web:25fea1e9cf538e6ad8ad18"
+    apiKey: "AIzaSyCbXf7G3FjpSuUD6stKKLtH1diL3Xk-Mho",
+    authDomain: "twitter-clone-68e92.firebaseapp.com",
+    projectId: "twitter-clone-68e92",
+    storageBucket: "twitter-clone-68e92.appspot.com",
+    messagingSenderId: "685874326853",
+    appId: "1:685874326853:web:ced44e3eb364384872a22c"
 };
 
 const app = initializeApp(firebaseConfig);
